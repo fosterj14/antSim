8e0a28da32f23e5b12bb87446c28ebf4a08b483a
John Foster <johnfost.361@gmail.com>
2021-08-05 14:12:39 -0400

    Added time tracker, random spawn of home nest

--- ant.py
+++ ant.py
@@ -5,34 +5,40 @@ class ant:
     name = "dasd"
     role = "asdas"
     inventory = 0
-    def __init__(self):
+    def __init__(self, homeX, homeY):
         #pick random role, either gatherer or worker
-        name = (random.choice(letters) for i in range(random.randint(3, 8)))
+        name = "default"
         inventory = 0
         age = 0
+        xCoord = homeX
+        yCoord = homeY
     def printAntInfo(self):
-        print("Name: " + str(self.name) + " " + "Role: " + self.role + "\n")
+        print("Name: " + str(self.name) + " " + "Role: " + self.role + "\n" + "Location: " + str(self.xCoord) + " ," + str(self.yCoord))
 
 class gatherer(ant):
-    def __init__(self):
+    def __init__(self, homeX, homeY):
         #pick random role, either gatherer or worker
         letters = string.ascii_letters
         self.name = "".join(random.choice(letters) for i in range(random.randint(3, 8)))
         self.inventory = 0
         self.age = 0
         self.role = "Gatherer"
-    def action():
+        self.xCoord = homeX
+        self.yCoord = homeY
+    def action(self):
         #move around map
         temp = ""
         #if on resource square, collect it
 
 class worker(ant):
-    def __init__(self):
+    def __init__(self, homeX, homeY):
         letters = string.ascii_letters
         self.name = "".join(random.choice(letters) for i in range(random.randint(3, 8)))
         self.inventory = 0
         self.age = 0
         self.role = "Worker"
-    def action():
+        self.xCoord = homeX
+        self.yCoord = homeY
+    def action(self):
         temp = ""
         #if enough free resources, build expansion to nest
--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 from tkinter import *
+from random import seed
+from random import randint
 import ant
 #https://realpython.com/python-gui-tkinter/
 
-numWorkers = 0
-numGatherers = 0
-homeLocation = 0 #this will be a coordinate in the array
+numWorkers = 1
+numGatherers = 1
+homeLocationX = 0 #this will be a coordinate in the array
+homeLocationY = 0
 year = 1
 month = 1
-day = 1
+day = 0
+yearChange = True
 count = 0
 antList = []
 
 def gameArray():
-    rows, cols = (10, 50)
-    gameArea = [[0 for i in range(cols)] for j in range(rows)]
+    rows, cols = (30, 70)
+    gameArea = [["-" for i in range(cols)] for j in range(rows)]
+    homeLocationX = randint(0, 30)
+    homeLocationY = randint(0, 70)
+    gameArea[homeLocationX][homeLocationY] = "H"
     return gameArea
 
-def updateGameArea(): #this will be called once after the ant action phase
-    #global count #testing
-    #gameArray[count%10][count%49] = 'X' #testing
-    #go through the list of ants and call their action function
+def updateGameArea(day, month, year, yearChange):
     print("update") #testing
+    #Time management
+    day = day + 1
+    if (day%31 == 0):
+        month = month + 1
+    if (month%13 == 0 and day%31 == 0):
+        year = year + 1
+        yearChange = True
+
+    #ant action call here
+    for ant in antList:
+        ant.action()
+
     str2 = ""
     for ele in gameArray:
         for ele2 in ele:
             str2 += str(ele2)
+            str2 += " "
         str2 += "\n"
-    playAreaText.config(text=str2)
-    #count +=1 #testing
-    window.after(1000, updateGameArea) #this can be moved to the ant action method
+    str2 = str2.rstrip("\n")
+
+    playAreaText.config(text=str2, fg = "white", bg = "black", font='Helvetica 12 bold')
+
+    infoText = "Workers: " + str(numWorkers) + "\t" + "Year: " + str(year) + "\n" + "Gatherers: " + str(numGatherers) + "\t" + "Month: " + str(month) + "\n" \
+    + "Home Location: " + str(homeLocationX) + " ," + str(homeLocationY) + "\t" + "Day: " + str(day)
+    infoSection.config(text = infoText)
+
+    window.after(1000, updateGameArea, day, month, year, yearChange)
 
 #Initializes the game window------------------------------------------------------------------------------------------
 window = Tk()
 window.resizable(width=FALSE, height=FALSE)
-window.geometry("600x600")
+window.geometry("1000x800")
 window.title("Ant Sim")
 #---------------------------------------------------------------------------------------------------------------------
 
@@ -45,31 +68,31 @@ playAreaFrame = Frame(height=600, width=600)
 
 infoLabel = Label(gameInfoFrame, text = "Hive information", foreground="white", background="black")
 
-infoSection = Text(gameInfoFrame, height = 4, width = 35)
-
 #need to format text in label here
 hiveInfo = "Workers: " + str(numWorkers) + "\t" + "Year: " + str(year) + "\n" + "Gatherers: " + str(numGatherers) + "\t" + "Month: " + str(month) + "\n" \
-    + "Home Location: " + str(homeLocation) + "\t" + "Day: " + str(day)
+    + "Home Location: " + str(homeLocationX) + " ," + str(homeLocationY) + "\t" + "Day: " + str(day)
+
+infoSection = Label(gameInfoFrame, text = hiveInfo, height = 4, width = 35)
 
 pauseBtn = Button(master=gameInfoFrame, text="Pause", width=5, height=1, bg="red", fg="white")
 
-playAreaText = Label(playAreaFrame)
+playAreaText = Label(playAreaFrame, fg = "white", bg = "black")
 #------------------------------------------------------------------------------------------------------------------------
 
-#prints the array to the text area
+#prints the array to the text area---------------------
 str1 = ""
 for ele in gameArray:
     for ele2 in ele:
         str1 += str(ele2)
     str1 += "\n"
+str1 = str1.rstrip("\n")
 playAreaText.config(text=str1)
-#------------------------------------------
+#---------------------------------------------------------
 
 #Create the starting ants------------------------------
-antList.append(ant.gatherer())
-antList.append(ant.worker())
+antList.append(ant.gatherer(homeLocationX, homeLocationY))
+antList.append(ant.worker(homeLocationX, homeLocationY))
 print("Printing info...")
-print(antList)
 for ant in antList:
     ant.printAntInfo()
 #------------------------------------------------------
@@ -77,7 +100,6 @@ for ant in antList:
 #Loads UI elements------------------------------------
 infoLabel.pack()
 infoSection.pack()
-infoSection.insert(END, hiveInfo)
 pauseBtn.pack()
 playAreaText.pack()
 
@@ -88,5 +110,5 @@ gameInfoFrame.pack(side=TOP)
 playAreaFrame.pack()
 #------------------------------------------------------
 
-updateGameArea()
+updateGameArea(day, month, year, yearChange)
 window.mainloop()
