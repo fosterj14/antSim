27e62cfc29daa2e5c27080238be4c9dbe07cb302
John Foster <johnfost.361@gmail.com>
2021-08-09 12:22:04 -0400

    adding to dev branch

--- ant.py
+++ ant.py
@@ -12,6 +12,7 @@ class ant:
         age = 0
         xCoord = homeX
         yCoord = homeY
+        deathFlag = False
     def printAntInfo(self):
         print("Name: " + str(self.name) + " " + "Role: " + self.role + "\n" + "Location: " + str(self.xCoord) + " ," + str(self.yCoord))
 
@@ -21,24 +22,26 @@ class gatherer(ant):
         letters = string.ascii_letters
         self.name = "".join(random.choice(letters) for i in range(random.randint(3, 8)))
         self.inventory = 0
-        self.age = 0
+        self.age = 0 #ant can die after some number of months
         self.role = "Gatherer"
         self.xCoord = homeX
         self.yCoord = homeY
     def action(self):
         #move around map
-        temp = ""
+            #if the element of where the and will move is "-", then it can move there, if not then it needs to look somewhere else
         #if on resource square, collect it
+            #if it moves onto a space that has the resource char on it, then it can pick it up
+        return
 
 class worker(ant):
     def __init__(self, homeX, homeY):
         letters = string.ascii_letters
         self.name = "".join(random.choice(letters) for i in range(random.randint(3, 8)))
         self.inventory = 0
-        self.age = 0
+        self.age = 0 #ant can die after some number of months
         self.role = "Worker"
         self.xCoord = homeX
         self.yCoord = homeY
     def action(self):
-        temp = ""
         #if enough free resources, build expansion to nest
+        return
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from random import seed
 from random import randint
 import ant
+import node
 #https://realpython.com/python-gui-tkinter/
 
 numWorkers = 1
@@ -11,19 +12,25 @@ homeLocationY = 0
 year = 1
 month = 1
 day = 0
-yearChange = True
 count = 0
 antList = []
 
 def gameArray():
     rows, cols = (30, 70)
-    gameArea = [["-" for i in range(cols)] for j in range(rows)]
-    homeLocationX = randint(0, 30)
-    homeLocationY = randint(0, 70)
-    gameArea[homeLocationX][homeLocationY] = "H"
+    #gameArea = [["-" for i in range(cols)] for j in range(rows)]
+
+    gameArea = [[node.node() for i in range(cols)] for j in range(rows)]
+
+    homeLocationX = randint(0, 29)
+    homeLocationY = randint(0, 69)
+    #gameArea[homeLocationX][homeLocationY] = "H"
+
+    gameArea[homeLocationX][homeLocationY].unit = "H"
+    gameArea[homeLocationX][homeLocationY].occupied = True
+
     return gameArea
 
-def updateGameArea(day, month, year, yearChange):
+def updateGameArea(day, month, year):
     print("update") #testing
     #Time management
     day = day + 1
@@ -31,34 +38,49 @@ def updateGameArea(day, month, year, yearChange):
         month = month + 1
     if (month%13 == 0 and day%31 == 0):
         year = year + 1
-        yearChange = True
 
     #ant action call here
     for ant in antList:
         ant.action()
+        ant.age = ant.age + 1
+        if (ant.age >= 250):
+            antList.remove(ant)
+        ant.printAntInfo()
+
+    spawnResources() #this will add new resorces to the map
 
     str2 = ""
-    for ele in gameArray:
+    for ele in gameArray: #gets the updated array info
         for ele2 in ele:
-            str2 += str(ele2)
+            #str2 += str(ele2)
+            str2 += str(ele2.unit)
             str2 += " "
         str2 += "\n"
     str2 = str2.rstrip("\n")
 
-    playAreaText.config(text=str2, fg = "white", bg = "black", font='Helvetica 12 bold')
+    playAreaText.config(text=str2, fg = "white", bg = "black", font='Helvetica 12 bold') #updates the play area
 
     infoText = "Workers: " + str(numWorkers) + "\t" + "Year: " + str(year) + "\n" + "Gatherers: " + str(numGatherers) + "\t" + "Month: " + str(month) + "\n" \
     + "Home Location: " + str(homeLocationX) + " ," + str(homeLocationY) + "\t" + "Day: " + str(day)
-    infoSection.config(text = infoText)
+    infoSection.config(text = infoText) #updates the hive info
 
-    window.after(1000, updateGameArea, day, month, year, yearChange)
+    window.after(1000, updateGameArea, day, month, year) #calls updateGameArea every 1000 msec
+
+def spawnResources():
+    #spawn resources in random areas
+    return
 
 #Initializes the game window------------------------------------------------------------------------------------------
 window = Tk()
+backgroundImg = PhotoImage("antPic.jpg")
 window.resizable(width=FALSE, height=FALSE)
 window.geometry("1000x800")
 window.title("Ant Sim")
 #---------------------------------------------------------------------------------------------------------------------
+
+backgroundImg = PhotoImage(file = 'E:\\python_work\\antSim\\antPic.PNG')
+canvas = Canvas(window, width = 1000, height = 800)
+canvas.create_image(0, 0, image = backgroundImg)
 
 gameArray = gameArray()
 
@@ -83,7 +105,9 @@ playAreaText = Label(playAreaFrame, fg = "white", bg = "black")
 str1 = ""
 for ele in gameArray:
     for ele2 in ele:
-        str1 += str(ele2)
+        #str1 += str(ele2)
+        str1 += str(ele2.unit)
+        str1 += " "
     str1 += "\n"
 str1 = str1.rstrip("\n")
 playAreaText.config(text=str1)
@@ -98,17 +122,18 @@ for ant in antList:
 #------------------------------------------------------
 
 #Loads UI elements------------------------------------
-infoLabel.pack()
-infoSection.pack()
-pauseBtn.pack()
-playAreaText.pack()
+infoLabel.pack() #just reads "Hive Information"
+infoSection.pack() #contains all the info on the hive
+pauseBtn.pack() #the pause button
+playAreaText.pack() #area where the game array is displayed
 
 infoSection.config(state=DISABLED)
 playAreaText.config(state=DISABLED, bg="white", fg="black")
 
 gameInfoFrame.pack(side=TOP)
 playAreaFrame.pack()
+canvas.pack(fill = "both", expand = True)
 #------------------------------------------------------
 
-updateGameArea(day, month, year, yearChange)
+updateGameArea(day, month, year)
 window.mainloop()
